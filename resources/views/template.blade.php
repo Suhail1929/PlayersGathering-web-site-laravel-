69bb056e6552630b2614bdae39a140c906e79150
garz0002 <alana.garzandat@etudiant.univ-reims.fr>
2021-12-08 14:40:50 +0100

    modif

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" type="image/x-icon" href="https://cdn2.iconfinder.com/data/icons/basic-ui-elements-16/117/gamepad-256.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('image/playersgathering.png')}}" />
     <!-- CSS Files -->
     <link rel="stylesheet" type="text/css" href="{{asset('css/design.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
